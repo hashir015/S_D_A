5f0186c4cde87902789b7818cba46f77a15fe651
Muhammad Hashir <135766257+hashir015@users.noreply.github.com>
2024-11-08 02:06:24 +0500

    Delete Online shopping system/src directory

--- Online shopping system/src/infrastructure/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package infrastructure;
-
-public class PaymentService {
-    public boolean processPayment(double amount) {
-        System.out.println("Processing payment of $" + amount + "...");
-        return true; // Assume payment is always successful
-    }
-}
